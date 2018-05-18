509c1e2eca895e5f119f2d25151a1363e1b41238
QuantumArcade <maxmarchionda@gmail.com>
2018-05-17 20:10:42 -0500

    messing with routes

---
+++ src/components/DevTest.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dev">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DevTest',
+  data() {
+    return {
+      msg: 'You think you know what you don\'t know.',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
