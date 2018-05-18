a5120d647ecbd8daf7572351f52c3989eecef257
QuantumArcade <maxmarchionda@gmail.com>
2018-05-17 20:10:36 -0500

    messing with routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import DevTest from '@/components/DevTest';
+
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/dev',
+      name: 'DevTest',
+      component: DevTest,
+    },
+    {
+      path: '*',
       name: 'HelloWorld',
       component: HelloWorld,
     },
